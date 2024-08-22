b50ad13745c78db6da4722fde0ef5f0430b91bf2
LorenzoAgnolucci <lory.agnolucci@gmail.com>
2024-08-22 14:00:14 +0900

    fix bug

--- clip/model.py
+++ clip/model.py
@@ -148,8 +148,8 @@ class ModifiedResNet(nn.Module):
     def forward(self, x, return_token=False, pos_embedding=False):
         def stem(x):
             x = self.relu1(self.bn1(self.conv1(x)))
-            x = self.relu2(self.bn2(self.conv2, x))
-            x = self.relu3(self.bn3(self.conv3, x))
+            x = self.relu2(self.bn2(self.conv2(x)))
+            x = self.relu3(self.bn3(self.conv3(x)))
             x = self.avgpool(x)
             return x
 
